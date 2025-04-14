bacdbaf5d7365e593ee1b2c220c40051f4fd5572
walidos195 <96259579+walidos195@users.noreply.github.com>
2025-04-14 22:57:49 +0200

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname)); // sert index.html à la racine
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});

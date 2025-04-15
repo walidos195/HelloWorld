202946b13792a0eb5ea0174634d29a46b8a1e86c
walidos195 <96259579+walidos195@users.noreply.github.com>
2025-04-15 02:21:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.use(express.static(__dirname)); // sert index.html à la racine
 
 const PORT = process.env.PORT || 3000;
